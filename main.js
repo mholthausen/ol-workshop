0367cd4ea481dcc520dc9babdbfe4069b50311ab
Michael Holthausen <holthausen@terrestris.de>
2018-10-19 14:46:52 +0200

    Rendering of elevation

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
 
+// Default public key from mapbox.com
+const key = "pk.eyJ1IjoibWhvbHRoYXVzZW4iLCJhIjoiY2puZnp1M2x4MDU3ajN2cW41dWc1emJ0YiJ9.HsBhoZy9z5WeQuyuVJgY1g"
+
+// Terrain-RGB 
+const elevation = new XYZSource({
+    url: 'https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=' + key,
+    crossOrigin: 'anonymous'
+  });
+
+// Neuer Tile Layer, der die Terrain-Quelle 'elevation' nutzt
 
 new Map({
     target: 'map-container',
@@ -13,7 +23,12 @@ new Map({
             source: new XYZSource({
                 url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
             })
-        })
+        }),
+
+        new TileLayer({
+            opacity: 0.8,
+            source: elevation
+          }),
     ],
     // Startposition und Zoomstufe angeben
     view: new View({
